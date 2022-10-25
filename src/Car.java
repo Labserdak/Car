a516c85490e84b1e6430a0d4361f07729ffd59a2
Labserdak <112883100+Labserdak@users.noreply.github.com>
2022-10-25 20:15:10 +0300

    Add files via upload

---
+++ src/Car.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class Car extends Thread {
+
+
+    public void go (int km ) throws InterruptedException {
+        int sr;
+        Random random = new Random();
+        int p = 0;
+        int t = 0;
+        int sec;
+        float speed = 1;
+        float r;
+
+        for (float i = km * 1000; i > 0; i -= r) {
+            speed += random.nextInt(8);
+
+            while (speed > 120) {
+                speed -= 1;
+            }
+            p += speed;
+            sr = p / t;
+            t ++;
+
+            r = (float) (speed * 0.28);
+            sec = (int) (i / (speed * 0.28));
+
+            System.out.println("Скорость (км/ч):    Средняя скорость (км/ч):    Оставшийся путь(км):    Оставшееся время(сек):" +
+                    "    Время пути(сек):");
+            System.out.println("     " + speed + "                     " + sr + "                      " + i / 1000 +
+                    "                      " + sec + "                    " + t);
+            System.out.println();
+
+            Thread.sleep(1000);
+
+        }
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+        Car car = new Car();
+        car.go(4);
+    }
+}
